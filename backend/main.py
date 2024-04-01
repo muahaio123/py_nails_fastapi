b861b7afcf7d552db4e9a428c08cd6ea3f6382e2
muahaio123 <longle9a@gmail.com>
2024-03-31 23:10:00 -0500

    added launch.json profile, changed to py 3.12.2 and simplify main and database.py

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from service.employees import *
 import json
 from types import SimpleNamespace
 
-# run the program with this command: python -m uvicorn main:app --reload
+# run the program with this command: py -m uvicorn main:app --reload
 
 create_connection()
 app = FastAPI()
@@ -13,23 +13,27 @@ app = FastAPI()
 # APIs for Employees
 @app.get("/employees")
 async def get_all_employees():
-    return json.dumps(select_all_employees().__dict__)
+    return json.dumps(select_all_employees())
 
 @app.get("/employees/{emp_id}")
 async def get_employee_id(emp_id: int):
-    return json.dumps(select_employee_id(emp_id).__dict__)
+    return json.dumps(select_employee_id(emp_id))
 
 @app.post("/employees")
 async def post_employee(request: Request):
     data = await request.json()
     new_emp = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
     new_emp = create_new_employee(new_emp)
-    return json.dumps(new_emp.__dict__)
+    return json.dumps(new_emp)
 
 @app.put("/employees")
 async def put_employee(request: Request):
+    data = await request.json()
+    exist_emp = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+    exist_emp = update_existing_employee(exist_emp)
+    return json.dumps(exist_emp)
     
 
 @app.delete("/employees/{emp_id}")
 async def delete_employees(emp_id: int):
-    return json.dumps(delete_existing_employee(emp_id).__dict__)
+    return json.dumps(delete_existing_employee(emp_id))
--- backend/service/database.py
+++ backend/service/database.py
@@ -1,20 +1,22 @@
 import sqlite3
 
-conn: sqlite3.Connection = None
+conn: sqlite3.Connection = sqlite3.connect("../natural_nails.db")
 def create_connection() -> sqlite3.Connection:
+    global conn
+    
     # create a database connection to a SQLite database (return existing one if have)
-    if not conn:
-        try:
-            conn = sqlite3.connect("../natural_nails.db")
-        except sqlite3.Error as e:
-            print(e)
+    try:
+        conn = sqlite3.connect("../natural_nails.db")
+    except sqlite3.Error as e:
+        print(e)
+
     return conn
 
 def getDBConnection():
-    return conn if conn else create_connection()
+    return create_connection()
 
 def getDBCursor():
-    return conn.cursor() if conn else create_connection().cursor()
+    return create_connection().cursor()
 
 def close_connection():
     if conn:
